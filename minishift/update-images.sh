5d0f418d7f4e6eb9f5d7e614621b7e7830fb32d9
Andrew Azores <me@andrewazor.es>
2019-04-29 12:29:30 -0400

    Correct Docker image 'from' spec to include docker.io

--- minishift/update-images.sh
+++ minishift/update-images.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 for i in robotmaker robotcontroller robotshop container-jmx-client; do
-    oc import-image andrewazores/$i:latest --from=andrewazores/$i --confirm
+    oc import-image andrewazores/$i:latest --from=docker.io/andrewazores/$i --confirm
 done
