0bbf9b73cc9a4529d2328822a7864a71d450cead
Andrew Azores <me@andrewazor.es>
2019-05-15 11:47:02 -0400

    Configure jmx-client routes for WS command channel and exporter

--- minishift/setup.sh
+++ minishift/setup.sh
@@ -13,4 +13,12 @@ oc expose svc container-jmc-web --port=8080
 
 oc new-app docker.io/andrewazores/container-jmx-client --name=jmx-client
 
+oc set env dc/jmx-client CONTAINER_DOWNLOAD_PORT="8080"
+
+oc expose dc jmx-client --name=jmx-client-exporter --port=8080
+
+oc expose svc jmx-client-exporter
+
 oc expose svc jmx-client --port=9090
+
+oc set env dc/jmx-client CONTAINER_DOWNLOAD_HOST="$(oc get route/jmx-client-exporter -o json | jq -r '.spec.host')"
