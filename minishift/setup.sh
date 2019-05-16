759a28e2571e1cac1925a6dc9b97c371b9520257
Andrew Azores <me@andrewazor.es>
2019-05-16 11:01:59 -0400

    Configure  services/routes and environment variables for automatic web-client connections

--- minishift/setup.sh
+++ minishift/setup.sh
@@ -9,7 +9,11 @@ for i in robotmaker robotcontroller robotshop container-jmc-web; do
     oc new-app docker.io/andrewazores/$i --name=$i
 done
 
-oc expose svc container-jmc-web --port=8080
+oc delete svc container-jmc-web
+
+oc expose dc container-jmc-web --target-port=8080 --port=80
+
+oc expose svc container-jmc-web
 
 oc new-app docker.io/andrewazores/container-jmx-client --name=jmx-client
 
@@ -21,4 +25,10 @@ oc expose svc jmx-client-exporter
 
 oc expose svc jmx-client --port=9090
 
-oc set env dc/jmx-client CONTAINER_DOWNLOAD_HOST="$(oc get route/jmx-client-exporter -o json | jq -r '.spec.host')"
+CLIENT_URL="$(oc get route/jmx-client-exporter -o json | jq -r '.spec.host')"
+
+WS_CLIENT_URL="ws://$(oc get route/jmx-client -o json | jq -r '.spec.host')/command"
+
+oc set env dc/jmx-client CONTAINER_DOWNLOAD_HOST="$CLIENT_URL"
+
+oc set env dc/container-jmc-web CONTAINER_JMX_CLIENT_URL="$WS_CLIENT_URL"
