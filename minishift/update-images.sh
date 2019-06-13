bb7b3c7040bd3b482e4becb33dbeea283ecd149d
Andrew Azores <me@andrewazor.es>
2019-06-13 11:27:13 -0400

    Remove s2i projects from update-images script

--- minishift/update-images.sh
+++ minishift/update-images.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-for i in robotmaker robotcontroller robotshop container-jmx-client container-jmc-web; do
+for i in container-jmx-client container-jmc-web; do
     oc import-image andrewazores/$i:latest --from=docker.io/andrewazores/$i --confirm
 done
